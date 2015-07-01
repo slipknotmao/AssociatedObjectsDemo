ee38b20fedf4f9af4442082e664f8c56eaa117c9
华晓春 <huaxiaochun1@huaxiaochundeiMac.local>
2015-07-01 15:48:20 +0800

    Initial Commit

---
+++ AssociatedObjectsDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AssociatedObjectsDemo
+//
+//  Created by 华晓春 on 15/7/1.
+//  Copyright (c) 2015年 华晓春. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AssociatedObjectsDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AssociatedObjectsDemo
+//
+//  Created by 华晓春 on 15/7/1.
+//  Copyright (c) 2015年 华晓春. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
